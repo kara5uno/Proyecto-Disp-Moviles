2a339b50106cdcfd39d1cf0ab7dcf9cc836d2901
wiritos1 <30850872+wiritos1@users.noreply.github.com>
2023-06-06 17:07:15 -0600

    make scrollable

--- Cartera movil/Cartera movil/WalletPage.xaml.cs
+++ Cartera movil/Cartera movil/WalletPage.xaml.cs
@@ -12,14 +12,46 @@ namespace Cartera_movil
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class WalletPage : ContentPage
     {
-        string user;
+        private CarterasDeUsuario wallets = new CarterasDeUsuario();
         string wallet;
         public WalletPage(string userName, string walletName)
         {
-            user = userName;
+            wallets.GetDataOfUser(userName);
             wallet = walletName;
             InitializeComponent();
+            ReloadWallets();
             carteraActual.Text= wallet;
+        }
+        private void ReloadWallets()
+        {
+            var myList = wallets.carteras; //your list here
+            carteras.Children.Clear(); //just in case so you can call this code several times np..
+            foreach (var item in myList)
+            {
+                var btn = new Button()
+                {
+                    Text = item.name + "  " + item.dinero.ToString(), //Whatever prop you wonna put as title;
+                    StyleId = item.name
+                };
+                btn.Clicked += ClickWallet;
+                carteras.Children.Add(btn);
+            }
+        }
+        private void ClickWallet(object sender, EventArgs e)
+        {
+            var myBtn = sender as Button;
+            var name = myBtn.StyleId;
+            return;
+        }
+        private void Return(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new MainMenu(wallets.user);
+        }
+
+        private void EnterMoney(object sender, EventArgs e)
+        {
+            wallets.AddMoney(wallet,(float)Convert.ToDouble(agregar.Text));
+            ReloadWallets();
         }
     }
 }
--- Cartera movil/Cartera movil/obj/Debug/netstandard2.0/WalletPage.xaml.g.cs
+++ Cartera movil/Cartera movil/obj/Debug/netstandard2.0/WalletPage.xaml.g.cs
@@ -20,12 +20,16 @@ namespace Cartera_movil {
         private global::Xamarin.Forms.Label carteraActual;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry agregar;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private global::Xamarin.Forms.StackLayout carteras;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(WalletPage));
             carteraActual = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "carteraActual");
+            agregar = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "agregar");
             carteras = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.StackLayout>(this, "carteras");
         }
     }
