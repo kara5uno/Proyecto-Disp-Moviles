fa91ec70ee00d6971a58a2b4f9716dc2a0cfd9b0
wiritos1 <30850872+wiritos1@users.noreply.github.com>
2023-06-05 23:33:20 -0600

    small text changes

--- Cartera movil/Cartera movil/obj/Debug/netstandard2.0/MoneyDeposit.xaml.g.cs
+++ Cartera movil/Cartera movil/obj/Debug/netstandard2.0/MoneyDeposit.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Cartera_movil {
     public partial class MoneyDeposit : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry money;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MoneyDeposit));
+            money = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "money");
         }
     }
 }
--- Cartera movil/Cartera movil/obj/Debug/netstandard2.0/TransferMoney.xaml.g.cs
+++ Cartera movil/Cartera movil/obj/Debug/netstandard2.0/TransferMoney.xaml.g.cs
@@ -17,8 +17,12 @@ namespace Cartera_movil {
     public partial class TransferMoney : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry money;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(TransferMoney));
+            money = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "money");
         }
     }
 }
