536df9016c384fa3575c4c152cf3d2cd104affe7
wiritos1 <30850872+wiritos1@users.noreply.github.com>
2023-06-07 15:19:03 -0600

    Acomodo botones

--- Cartera movil/Cartera movil/WalletPage.xaml.cs
+++ Cartera movil/Cartera movil/WalletPage.xaml.cs
@@ -28,19 +28,47 @@ namespace Cartera_movil
             carteras.Children.Clear(); //just in case so you can call this code several times np..
             foreach (var item in myList)
             {
-                if(item.name==wallet)
+                var grd = new Grid
                 {
-                    continue;
-                }
-                var btn = new Button()
+                    RowDefinitions =
+                    {
+                        new RowDefinition()
+                    },
+                    ColumnDefinitions =
+                    {
+                        new ColumnDefinition(),
+                        new ColumnDefinition(),
+                        new ColumnDefinition(),
+                        new ColumnDefinition(),
+                        new ColumnDefinition(),
+                        new ColumnDefinition(),
+                        new ColumnDefinition(),
+                    },
+                    BackgroundColor = Xamarin.Forms.Color.FromRgb(100, 104, 110)
+                };
+                Button btn = new Button
                 {
-                    Text = item.name + "  " + item.dinero.ToString(), //Whatever prop you wonna put as title;
-                    StyleId = item.name
+                    StyleId = item.name,
+                    VerticalOptions = LayoutOptions.FillAndExpand,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    BackgroundColor = Color.FromRgba(0, 0, 0, 0),
                 };
                 btn.Clicked += ClickWallet;
-                carteras.Children.Add(btn);
+                grd.Children.Add(new Label
+                {
+                    Text = item.name,
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                }, 1, 4, 0, 1); ;
+                grd.Children.Add(new Label
+                {
+                    Text = "$" + item.dinero.ToString(),
+                    VerticalOptions = LayoutOptions.CenterAndExpand,
+                }, 5, 6, 0, 1);
+                grd.Children.Add(btn, 0, 7, 0, 1);
+                carteras.Children.Add(grd);
             }
         }
+
         private async void ClickWallet(object sender, EventArgs e)
         {
             var myBtn = sender as Button;
